03b5718191d39a570d6d8ea4810dd0d9234e2c72
luisguilheerme <lg.maroni@gmail.com>
2024-01-19 10:04:22 -0300

    Project Created

---
+++ src/main/java/com/luisguilherme/apireceitas/ApiReceitasApplication.java
@@ -0,0 +1,13 @@
+package com.luisguilherme.apireceitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiReceitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiReceitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisguilherme/apireceitas/ApiReceitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisguilherme.apireceitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReceitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
